d3479a9090e7a4334cbf873ed6c808845b577b3d
Yuri Glushenkov <1arxemond1@gmail.com>
2019-06-10 22:32:05 +0300

    example for memory leaks detection has been added jxray HeapDump analyzer

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # spring_jmc_jmeter_profiling
+
+```bash
+-Xmx1g -Xms1g -verbose:gc -XX:+PrintGC -XX:+HeapDumpOnOutOfMemoryError -XX:+UseConcMarkSweepGC -XX:HeapDumpPath="./memleaktest.hprof" -XX:OnOutOfMemoryError="kill -9 %p"
+```
+http://www.jxray.com/download/
+```bash
+sh jxray.sh ~/IdeaProjects/spring_jmc_jmeter_profiling/memleaktest.hprof ~/IdeaProjects/spring_jmc_jmeter_profiling/memleaks.html
+```
--- src/main/java/example/controller/GreetingController.java
+++ src/main/java/example/controller/GreetingController.java
@@ -1,17 +1,26 @@
 package example.controller;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import example.dto.Greeting;
 import example.dto.MyClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import static java.util.stream.Collectors.joining;
 
 @RestController
 public class GreetingController {
@@ -20,6 +29,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
     private final String URL = "http://arxemond.ru";
     private final List<MyClass> myClasses = new ArrayList<>();
+    private final List<String> stringList = new LinkedList<>();
 
     @Autowired
     private RestTemplate restTemplate;
@@ -44,5 +54,24 @@ public class GreetingController {
     @RequestMapping("/without-delay")
     public String withoutDelay() {
         return restTemplate.getForEntity(URL, String.class).getBody();
+    }
+
+    @RequestMapping("/memory-leak")
+    public ResponseEntity<?> memoryLeak() {
+        final String string = IntStream.range(0, 50000)
+                .boxed()
+                .map(idx ->
+                        CompletableFuture.supplyAsync(() -> {
+                            final String s = String.valueOf(idx);
+                            stringList.add(s);
+                            return s;
+                        }))
+                .map(CompletableFuture::join)
+                .collect(joining(" "));
+
+        stringList.add(string);
+
+        return new ResponseEntity<>(string, HttpStatus.OK);
+
     }
 }
